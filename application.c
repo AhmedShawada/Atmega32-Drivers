f9b002f956f687f47d0657cfc3ef0fa51d135f9f
AhmedShawada <ashawada7@gmail.com>
2025-02-11 20:50:35 +0200

    Application files

---
+++ application.c
@@ -0,0 +1,34 @@
+/*
+ * File   :  application.c
+ * https://www.linkedin.com/in/ahmed-shawada/
+ * Author : AhmedShawada
+ * Created: 12/3/2024 4:02:19 AM
+*/ 
+
+#include "application.h"
+
+
+int main(void)
+{
+	
+	application_intialize();
+		
+	while(1)
+	{
+		
+		
+	}
+	
+	return 0;
+}
+
+void application_intialize (void)
+{
+	Std_ReturnType ret = E_OK;
+
+
+	if (ret != E_OK)
+	{/*Handle failure*/}
+}
+
+
